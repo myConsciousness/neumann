00ccbe7f190fc2dc10fbe6d0a4b06c64ff3f1986
Kato Shinya <yourdream28@gmail.com>
2020-11-21 15:06:53 +0900

    add

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"org.thinkit.neumann.catalog","l":"MathematicalConstant"},{"p":"org.thinkit.neumann","l":"Neumann"}];updateSearchResults();
