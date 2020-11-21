a2f7c505ae166776ede3ff07c9721fcd3fc35afe
Kato Shinya <yourdream28@gmail.com>
2020-11-21 15:05:32 +0900

    add

---
+++ src/main/java/org/thinkit/neumann/catalog/MathematicalConstant.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.neumann.catalog;
+
+import org.thinkit.api.catalog.Catalog;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 数学定数を管理するカタログです。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@RequiredArgsConstructor
+public enum MathematicalConstant implements Catalog<MathematicalConstant> {
+
+    /**
+     * 円周率 (3.14)
+     */
+    PI(0),
+
+    /**
+     * ネイピア数
+     */
+    NAPIER(1);
+
+    /**
+     * コード値
+     */
+    @Getter
+    private final int code;
+}
