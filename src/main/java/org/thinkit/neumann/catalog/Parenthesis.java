306ccc71298c1fe24e23ca7b0270b10b74b05a91
Kato Shinya <yourdream28@gmail.com>
2020-11-22 13:47:26 +0900

    add

---
+++ src/main/java/org/thinkit/neumann/catalog/Parenthesis.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.neumann.catalog;
+
+import org.thinkit.api.catalog.BiCatalog;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 丸括弧を管理するカタログです。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@RequiredArgsConstructor
+public enum Parenthesis implements BiCatalog<Parenthesis, String> {
+
+    /**
+     * 始め丸括弧
+     */
+    OPEN(0, "("),
+
+    /**
+     * 終わり丸括弧
+     */
+    CLOSE(1, ")");
+
+    /**
+     * コード値
+     */
+    @Getter
+    private final int code;
+
+    /**
+     * タグ
+     */
+    @Getter
+    private final String tag;
+}
