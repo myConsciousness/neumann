d0a1d8cebca435657b4de25bdfeb18d385b6d18d
Kato Shinya <yourdream28@gmail.com>
2020-11-22 22:15:04 +0900

    fixed structure

--- src/main/java/org/thinkit/neumann/catalog/MathematicalFunction.java
+++ src/main/java/org/thinkit/neumann/catalog/MathematicalFunction.java
@@ -14,7 +14,8 @@
 
 package org.thinkit.neumann.catalog;
 
-import org.thinkit.api.catalog.Catalog;
+import org.thinkit.api.catalog.BiCatalog;
+import org.thinkit.neumann.Function;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -27,116 +28,122 @@ import lombok.RequiredArgsConstructor;
  * @version 1.0
  */
 @RequiredArgsConstructor
-public enum MathematicalFunction implements Catalog<MathematicalFunction> {
+public enum MathematicalFunction implements BiCatalog<MathematicalFunction, Function> {
 
     /**
      * 少数点以下切り上げ
      */
-    CEIL(0),
+    CEIL(0, Function.of(FunctionPattern.CEIL)),
 
     /**
      * 少数点以下切り下げ
      */
-    FLOOR(1),
+    FLOOR(1, Function.of(FunctionPattern.FLOOR)),
 
     /**
      * 四捨五入
      */
-    ROUND(2),
+    ROUND(2, Function.of(FunctionPattern.ROUND)),
 
     /**
      * 絶対値
      */
-    ABS(3),
+    ABS(3, Function.of(FunctionPattern.ABS)),
 
     /**
      * 正弦
      */
-    SINE(4),
+    SINE(4, Function.of(FunctionPattern.SINE)),
 
     /**
      * 余弦
      */
-    COSINE(5),
+    COSINE(5, Function.of(FunctionPattern.COSINE)),
 
     /**
      * 正接
      */
-    TANGENT(6),
+    TANGENT(6, Function.of(FunctionPattern.TANGENT)),
 
     /**
      * 逆正弦
      */
-    ARC_SINE(7),
+    ARC_SINE(7, Function.of(FunctionPattern.ARC_SINE)),
 
     /**
      * 逆余弦
      */
-    ARC_COSINE(8),
+    ARC_COSINE(8, Function.of(FunctionPattern.ARC_COSINE)),
 
     /**
      * 逆正接
      */
-    ARC_TANGENT(9),
+    ARC_TANGENT(9, Function.of(FunctionPattern.ARC_TANGENT)),
 
     /**
      * 双曲線関数（正弦）
      */
-    HYPERBOLIC_SINE(10),
+    HYPERBOLIC_SINE(10, Function.of(FunctionPattern.HYPERBOLIC_SINE)),
 
     /**
      * 双曲線関数（余弦）
      */
-    HYPERBOLIC_COSINE(11),
+    HYPERBOLIC_COSINE(11, Function.of(FunctionPattern.HYPERBOLIC_COSINE)),
 
     /**
      * 双曲線関数（正接）
      */
-    HYPERBOLIC_TANGENT(12),
+    HYPERBOLIC_TANGENT(12, Function.of(FunctionPattern.HYPERBOLIC_TANGENT)),
 
     /**
      * 最小値
      */
-    MIN(13),
+    MIN(13, Function.of(FunctionPattern.MIN)),
 
     /**
      * 最大値
      */
-    MAX(14),
+    MAX(14, Function.of(FunctionPattern.MAX)),
 
     /**
      * 合計値
      */
-    SUM(15),
+    SUM(15, Function.of(FunctionPattern.SUM)),
 
     /**
      * 平均値
      */
-    AVERAGE(16),
+    AVERAGE(16, Function.of(FunctionPattern.AVERAGE)),
 
     /**
      * 自然対数
      */
-    LN(17),
+    LN(17, Function.of(FunctionPattern.LN)),
 
     /**
      * 常用対数
      */
-    LOG(18),
+    LOG(18, Function.of(FunctionPattern.LOG)),
 
     /**
      * 乱数
      */
-    RANDOM(19),
+    RANDOM(19, Function.of(FunctionPattern.RANDOM)),
 
     /**
      * 平方根
      */
-    SQRT(20);
+    SQRT(20, Function.of(FunctionPattern.SQRT));
 
     /**
      * コード値
      */
     @Getter
     private final int code;
+
+    /**
+     * タグ
+     */
+    @Getter
+    private final Function tag;
 }
